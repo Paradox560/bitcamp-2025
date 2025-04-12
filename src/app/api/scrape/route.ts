c55cb234886794151bbce1c8cf9c90680df485eb
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2025-04-12 00:27:49 -0400

    Flask app, clerk, firebase user creation, and started web scraping

---
+++ src/app/api/scrape/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        // Parse the JSON body from the request
+        const body = await req.json();
+
+        // Send body to Flask API
+        const response = await fetch('http://127.0.0.1:5000/api/solve', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+        });
+
+        // Receive response
+        const data = await response.json();
+        return NextResponse.json(data, { status: response.status });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to connect to Python backend' }, { status: 500 });
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,95 @@
-import Image from "next/image";
+'use client'
+
+import { SignedOut, SignedIn, useUser } from "@clerk/nextjs";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Parisienne, Marck_Script } from "next/font/google";
+
+// Proper font imports
+const parisienne = Parisienne({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const marck_Script = Marck_Script({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { user, isLoaded, isSignedIn } = useUser();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const createUser = async () => {
+    try {
+      const collectionRef = collection(db, 'users');
+      const docRef = doc(collectionRef, user?.id);
+      const docSnap = await getDoc(docRef);
+    
+      if (docSnap.exists()) {
+        console.log("User already exists in db.");
+      } else {
+        console.log("User does not exist in db. Creating a new user in db.");
+
+        // Add a new document with a generated id.
+        await setDoc(docRef, {
+          firstName: user?.firstName,
+          lastName: user?.lastName,
+          email: user?.primaryEmailAddress?.emailAddress,
+        })
+      }
+    } catch (error) {
+      let message
+      if (error instanceof Error) message = error.message
+      else message = String(error)
+      console.error("Error adding user to db: " + message);
+    }
+  }
+
+  useEffect(() => {
+    if(isSignedIn && isLoaded && user){
+      createUser();
+    } else {
+      console.log("Parameters not met to create" + isSignedIn + " " + user);
+    }
+  }, [isSignedIn, isLoaded, user]);
+
+  return (
+    <div className="min-h-screen w-full bg-[#860F09]">
+      <h1 className={`${parisienne.className} text-8xl pt-8 ml-7 w-fit text-[#FDEDD6] border-b-[0.15rem] border-white`}>
+        Welcome to MacroTerpitect
+      </h1>
+      <h2 className={`${marck_Script.className} text-5xl ml-10 mt-3 text-[#FDEDD6]`}>
+        Your Next Best Meal Preparation App
+      </h2>
+
+      <SignedIn>
+        <p className='text-2xl ml-[3%] mt-[5%] w-3/5 text-[#FDEDD6] font-light'>
+          Yada Yada Yada does something probably
+        </p>
+        <Button 
+          asChild
+          className='ml-[3%] mt-[6%] w-44 h-18 bg-[#B37238] text-[#FDEDD6] text-2xl font-normal hover:bg-[#9c632f]'
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Link href="/home">Home</Link>
+        </Button>
+      </SignedIn>
+      
+      <SignedOut>
+        <p className='text-2xl ml-[3%] mt-[5%] w-3/5 text-[#FDEDD6] font-light'>
+          Longer description of the app goes here
+        </p>
+        <Button 
+          asChild
+          className='ml-[3%] mt-[6%] w-44 h-18 bg-[#B37238] text-[#FDEDD6] text-2xl font-normal hover:bg-[#9c632f]'
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Link href="/sign-in">Log In</Link>
+        </Button>
+      </SignedOut>
     </div>
   );
 }
