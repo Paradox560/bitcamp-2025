557bed4be28b9954f3da0ab2df5cc69a3a04b0b3
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2025-04-13 07:16:35 -0400

    Local importing for vercel

--- src/flask-app/app.py
+++ src/flask-app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from ingredient_filtering import filter_ingredients
-from gemini_route import generate
+from .ingredient_filtering import filter_ingredients
+from .gemini_route import generate
 import json
 
 app = Flask(__name__)
